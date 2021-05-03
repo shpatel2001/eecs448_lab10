76843bcd756c8500c6d3e55ecebaaf7e211bd161
shpatel2001 <60803469+shpatel2001@users.noreply.github.com>
2021-05-02 23:36:58 -0500

    Add files via upload

---
+++ ViewUsers.php
@@ -0,0 +1,25 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "shayenpatel", "jae3ieW3", "shayenpatel");
+
+/* check connection */
+if ($mysqli->connect_errno) 
+{
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "SELECT user_id FROM Users;";
+   
+$result = $mysqli->query($query);
+   while ($row = $result->fetch_assoc())
+   {
+       echo "" . $row["user_id"] . "<br>";
+   }
+printf("image of user and posts on SQL table also attached to the file.");
+
+$result->free();
+ /* free result set */
+ 
+/* close connection */
+$mysqli->close();
+?> 
